f959f3342a9a025fb865e3d355373a5c058a61da
zizya <zzizya@gmail.com>
2020-02-16 22:01:33 +0200

    corect

--- index.js
+++ index.js
@@ -22,92 +22,59 @@ function convert() {
 function read(json) {
     let income = JSON.parse(json);
 
-        if( Number.isInteger(income.distance.value)){
-           
-        }
-        else{
-             alert('Введите число!');
-             return;
-        
+    if (Number.isInteger(income.distance.value)) {
+
+    }
+    else {
+        alert('Введите число!');
+        return;
+
     }
 
-let a = (income.distance.unit === 'mm' && income.conver_to === 'mm') ?  (income.distance.value * 1) :
-(income.distance.unit === 'mm' && income.conver_to === 'cm') ?  (income.distance.value / 10)  :
-(income.distance.unit === 'mm' && income.conver_to === 'm') ?  (income.distance.value / 1000) :
-(income.distance.unit === 'mm' && income.conver_to === 'km') ?  (income.distance.value / 1000000) :
-(income.distance.unit === 'mm' && income.conver_to === 'in') ?  (income.distance.value / 25.4) :
-(income.distance.unit === 'mm' && income.conver_to === 'ft') ?  (income.distance.value / 304.8) :
-(income.distance.unit === 'mm' && income.conver_to === 'yd') ?  (income.distance.value / 914.4) :
+    let unit = income.distance.unit;
+    let value = income.distance.value;
+    function convertToMM(unit, value) {
+        let b =
+            (unit === 'mm') ? (value * 1) :
+                (unit === 'cm') ? (value * 10) :
+                    (unit === 'm') ? (value * 1000) :
+                        (unit === 'km') ? (value * 1e+6) :
+                            (unit === 'in') ? (value * 25.4) :
+                                (unit === 'ft') ? (value * 304.8) :
+                                    (unit === 'yd') ? (value * 914.4) :
+                                        'Vse good';
 
-(income.distance.unit === 'cm' && income.conver_to === 'mm') ?  (income.distance.value * 10) :
-(income.distance.unit === 'cm' && income.conver_to === 'cm') ?  (income.distance.value * 1) :
-(income.distance.unit === 'cm' && income.conver_to === 'm') ?  (income.distance.value / 10) :
-(income.distance.unit === 'cm' && income.conver_to === 'km') ?  (income.distance.value / 100000) :
-(income.distance.unit === 'cm' && income.conver_to === 'in') ?  (income.distance.value / 2.54) :
-(income.distance.unit === 'cm' && income.conver_to === 'ft') ?  (income.distance.value / 30.48) :
-(income.distance.unit === 'cm' && income.conver_to === 'yd') ?  (income.distance.value / 91.44) :
+        let a =
+            (income.conver_to === 'mm') ? (b * 1) :
+                (income.conver_to === 'cm') ? (b / 10) :
+                    (income.conver_to === 'm') ? (b / 1000) :
+                        (income.conver_to === 'km') ? (b / 1000000) :
+                            (income.conver_to === 'in') ? (b / 25.4) :
+                                (income.conver_to === 'ft') ? (b / 304.8) :
+                                    (income.conver_to === 'yd') ? (b / 914.4) :
+                                        'vse good';
 
-(income.distance.unit === 'm' && income.conver_to === 'mm') ?  (income.distance.value * 1000) :
-(income.distance.unit === 'm' && income.conver_to === 'cm') ?  (income.distance.value * 100) :
-(income.distance.unit === 'm' && income.conver_to === 'm') ?  (income.distance.value * 1) :
-(income.distance.unit === 'm' && income.conver_to === 'km') ?  (income.distance.value / 1000) :
-(income.distance.unit === 'm' && income.conver_to === 'in') ?  (income.distance.value * 39.37) :
-(income.distance.unit === 'm' && income.conver_to === 'ft') ?  (income.distance.value * 3.281) :
-(income.distance.unit === 'm' && income.conver_to === 'yd') ?  (income.distance.value * 1.094) :
+        let result = {
+            unit: income.conver_to,
+            value: a.toFixed(5)
+        }
+        let again = JSON.stringify(result);
+        decode(again);
+    }
 
-(income.distance.unit === 'km' && income.conver_to === 'mm') ?  (income.distance.value * 1e+6) :
-(income.distance.unit === 'km' && income.conver_to === 'cm') ?  (income.distance.value * 100000) :
-(income.distance.unit === 'km' && income.conver_to === 'm') ?  (income.distance.value * 1000) :
-(income.distance.unit === 'km' && income.conver_to === 'km') ?  (income.distance.value * 1) :
-(income.distance.unit === 'km' && income.conver_to === 'in') ?  (income.distance.value * 39370.1) :
-(income.distance.unit === 'km' && income.conver_to === 'ft') ?  (income.distance.value * 3280.84) :
-(income.distance.unit === 'km' && income.conver_to === 'yd') ?  (income.distance.value * 1093.61) :
 
-(income.distance.unit === 'in' && income.conver_to === 'mm') ?  (income.distance.value * 25.4) :
-(income.distance.unit === 'in' && income.conver_to === 'cm') ?  (income.distance.value * 2.54) :
-(income.distance.unit === 'in' && income.conver_to === 'm') ?  (income.distance.value / 39.37) :
-(income.distance.unit === 'in' && income.conver_to === 'km') ?  (income.distance.value / 39370.1) :
-(income.distance.unit === 'in' && income.conver_to === 'in') ?  (income.distance.value * 1) :
-(income.distance.unit === 'in' && income.conver_to === 'ft') ?  (income.distance.value / 12) :
-(income.distance.unit === 'in' && income.conver_to === 'yd') ?  (income.distance.value / 36) :
 
-(income.distance.unit === 'ft' && income.conver_to === 'mm') ?  (income.distance.value * 304.8) :
-(income.distance.unit === 'ft' && income.conver_to === 'cm') ?  (income.distance.value * 30.48) :
-(income.distance.unit === 'ft' && income.conver_to === 'm') ?  (income.distance.value / 3.281) :
-(income.distance.unit === 'ft' && income.conver_to === 'km') ?  (income.distance.value / 3281) :
-(income.distance.unit === 'ft' && income.conver_to === 'in') ?  (income.distance.value * 12) :
-(income.distance.unit === 'ft' && income.conver_to === 'ft') ?  (income.distance.value * 1) :
-(income.distance.unit === 'ft' && income.conver_to === 'yd') ?  (income.distance.value / 3) :
+    function decode(again) {
+        let result2 = JSON.parse(again);
+        console.log(result2);
+        let screen = document.getElementById('resultat');
 
-(income.distance.unit === 'yd' && income.conver_to === 'mm') ?  (income.distance.value * 914.4) :
-(income.distance.unit === 'yd' && income.conver_to === 'cm') ?  (income.distance.value * 91.44) :
-(income.distance.unit === 'yd' && income.conver_to === 'm') ?  (income.distance.value / 1.094) :
-(income.distance.unit === 'yd' && income.conver_to === 'km') ?  (income.distance.value / 1093.61) :
-(income.distance.unit === 'yd' && income.conver_to === 'in') ?  (income.distance.value * 36) :
-(income.distance.unit === 'yd' && income.conver_to === 'ft') ?  (income.distance.value * 3) :
-(income.distance.unit === 'yd' && income.conver_to === 'yd') ?  (income.distance.value * 1) :
-'Vse good';
+        screen.innerHTML = result2.value + result2.unit;
+    }
 
-let result = {
-    unit: income.conver_to,
-    value: a.toFixed(5)
+    convertToMM(unit, value);
+
 }
-let again=JSON.stringify(result);
-decode(again);
-}
-
-function decode(again){
-    let result2=JSON.parse(again);
-    console.log(result2);
-    let screen = document.getElementById('resultat');
-
-    screen.innerHTML = result2.value +result2.unit;
-}
-
-
-
-
-
 
 
 
